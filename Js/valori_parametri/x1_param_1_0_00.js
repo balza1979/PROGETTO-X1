d3ea2529a2fb7ea71beb0d94fc371c0c1cbd0e15
balza1979 <balza1979@live.com>
2026-04-20 15:39:16 +0200

    Create x1_param_1_0_00.js

---
+++ Js/valori_parametri/x1_param_1_0_00.js
@@ -0,0 +1,10 @@
+const x1_param_1_0_00_raw = [
+  "\"\"\"00\"\" UPR cab/piano.  \"",
+  "\"\"\"01\"\" UPR cab/APBpi.\"",
+  "\"\"\"02\"\" APB cab/piano.  \"",
+  "\"\"\"03\"\" APB/SX cabina.  \"",
+  "\"\"\"04\"\" SX discesa.  \"",
+  "\"\"\"05\"\" SX sal./dis. \""
+];
+
+const x1_param_1_0_00 = x1_param_1_0_00_raw.map(v => v);
