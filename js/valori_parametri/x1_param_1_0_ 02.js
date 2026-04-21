c9561b6933877eb99840ce00b694bb0af6db8a50
balza1979 <balza1979@live.com>
2026-04-21 10:36:51 +0200

    Add files via upload

---
+++ js/valori_parametri/x1_param_1_0_ 02.js
@@ -0,0 +1,9 @@
+const x1_param_1_0_02_raw = [
+  "\"\"\"00\"\" No\"",
+  "\"\"\"01\"\" Prenotazioni\"",
+  "\"\"\"02\"\" Occupato\"",
+  "\"\"\"03\"\" Occ.+pren. Lamp. (max 16 fermate)\"",
+  "\"\"\"04\"\" Occ.lamp.mar.\""
+];
+
+const x1_param_1_0_02 = x1_param_1_0_02_raw.map(v => v);
