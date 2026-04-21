16f39ef3168195f654943550aeed9ab77fa4ae88
balza1979 <balza1979@live.com>
2026-04-21 17:48:09 +0200

    Delete js/valori_parametri/x1_param_1_0_01.js

--- js/valori_parametri/x1_param_1_0_01.js
+++
@@ -1,9 +0,0 @@
-const x1_param_1_0_01_raw = [
-  "\"\"\"00\"\" No  \"",
-  "\"\"\"01\"\" Prenotazioni \"",
-  "\"\"\"02\"\" Posizione (SOLO X K)\"",
-  "\"\"\"03\"\" Prenotazione / Posizione (SOLO X K)\""
-];
-
-const x1_param_1_0_01 = x1_param_1_0_01_raw.map(v => v);
-
