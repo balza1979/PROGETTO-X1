4ccdd48fe7ff92fd283da7d59fb40b1cf6952fb4
balza1979 <balza1979@live.com>
2026-04-21 17:58:45 +0200

    Reformat and clean up x1_param_1_0_00.js

--- js/valori_parametri/x1_param_1_0_00.js
+++ js/valori_parametri/x1_param_1_0_00.js
@@ -23,3 +23,4 @@ const file_parametro = {
   "04": ["1.JPG","2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG"],
   "05": ["1.JPG","2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG"]
 };
+
