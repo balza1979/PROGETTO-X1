1301e231e3413f5a694d58637f8d4263c58529b4
balza1979 <balza1979@live.com>
2026-04-21 18:01:50 +0200

    Remove commented code and clean up formatting

--- js/valori_parametri/x1_param_1_0_00.js
+++ js/valori_parametri/x1_param_1_0_00.js
@@ -23,4 +23,3 @@ const file_parametro = {
   "04": ["1.JPG","2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG"],
   "05": ["1.JPG","2.JPG","3.JPG","4.JPG","5.JPG","6.JPG","7.JPG","8.JPG"]
 };
-
